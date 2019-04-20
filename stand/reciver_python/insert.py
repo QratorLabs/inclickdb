714a6d0a0180babf0aca422fdee920da5fac7eb8
aleksandrashlychkova <aleksandrashlychkova@gmail.com>
2019-04-21 01:35:05 +0300

    test

--- stand/reciver_python/insert.py
+++ stand/reciver_python/insert.py
@@ -1,7 +1,40 @@
+import socket
 from clickhouse_driver.client import Client
+import time
 
-client = Client('clickhouse')
+sock = socket.socket()
+PORT = 2003
+sock.bind(("", PORT))
+taglist = []
 
-print(client.execute('INSERT INTO tmp \n\
+def parse_tag(tag_value):
+    tag = tag_value.split('=')[0]
+    value = float(tag_value.split('=')[1])
+    if tag not in taglist:
+        taglist.append(tag)
+    return tag, value
+
+
+if __name__ == "__main__":
+
+    client = Client('clickhouse')
+    print('aaaaaaaaaaaaaaaaa')
+    print(client.execute("CREATE DATABASE IF NOT EXISTS events"))
+    print(client.execute('CREATE TABLE events.tmp(\n\
+    path String,\n\
+    last_volume UInt32\n\
+    ) ENGINE = Log;'))
+    client.execute('INSERT INTO tmp \n\
+                (path, last_volume) VALUES\n\
+                ( \"tytyt\", 31)')
+    print(client.execute('SELECT * FROM events.tmp'))
+
+    print(client.execute('INSERT INTO tmp \n\
                         (path, last_volume) VALUES\n\
-                        ( \"tytyt\", 31)'))
+                        ( \"uuytyt\", 31)'))
+    print(client.execute('SELECT * FROM events.tmp'))
+    print('aaaaaaaaaaaaaaaaaaaaaa')
+    time.sleep(30)
+
+
+
