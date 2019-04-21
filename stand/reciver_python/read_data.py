77ad979131f519b938371e7d00ee8e404229ac63
aleksandrashlychkova <aleksandrashlychkova@gmail.com>
2019-04-21 14:25:04 +0300

    test

--- stand/reciver_python/read_data.py
+++ stand/reciver_python/read_data.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
             for tag_value in tmp[1:]:
                 tag, value = parse_tag(tag_value)
             print(client.execute('INSERT INTO events.tmp  (timestmp, path, last_volume) VALUES',
-                                 [{'timestmp': i, 'path': path, 'last_volume': i}]))
+                                 [{'timestmp': i, 'path': path, 'last_volume': value}]))
 
     print(client.execute('SELECT * FROM events.tmp'))
     time.sleep(30)
